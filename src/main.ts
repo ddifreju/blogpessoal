b06ac621afd4398915e4b2b92d0d9d10a34cd9f5
ddifreju <jufreddi@gmail.com>
2025-07-07 14:12:24 -0300

    Deploy do projeto blog pessoal

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Blog Pessoal da Juliana Freddi')
+  .setDescription('Projeto Blog Pessoal feito como atividade prática para o Bootcamp Generation')
+  .setContact("Juliana Freddi",
+    "https://github.com/ddifreju",
+    "juhfreddi@gmail.com")
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+
   process.env.TZ = '-03:00'
   app.useGlobalPipes (new ValidationPipe());
   app.enableCors();
--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -1,30 +1,36 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsEmail, IsNotEmpty, MinLength } from "class-validator"
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm"
 import { Postagem } from "../../postagem/entities/postagem.entity"
 
-@Entity({ name: "tb_usuarios" })
+@Entity({name: "tb_usuarios"})
 export class Usuario {
 
-    @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn() 
+    @ApiProperty() 
     id: number
 
     @IsNotEmpty()
-    @Column({ length: 255, nullable: false })
+    @Column({length: 255, nullable: false}) 
+    @ApiProperty() 
     nome: string
 
     @IsEmail()
-    @IsNotEmpty()
-    @Column({ length: 255, nullable: false })
+    @Column({length: 255, nullable: false })
+    @ApiProperty({example: "email@email.com.br"}) 
     usuario: string
 
-    @MinLength(8)
     @IsNotEmpty()
-    @Column({ length: 255, nullable: false })
+    @MinLength(8)
+    @Column({length: 255, nullable: false }) 
+    @ApiProperty({example: 'Mínimo de 8 caracteres'}) 
     senha: string
 
-    @Column({ length: 5000 })
+    @Column({length: 5000 }) 
+    @ApiProperty() 
     foto: string
 
+    @ApiProperty() 
     @OneToMany(() => Postagem, (postagem) => postagem.usuario)
     postagem: Postagem[]
 
